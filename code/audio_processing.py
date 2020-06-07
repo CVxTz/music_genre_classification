383887013e88af7480ba060b2358be7d53fbf6fe
YM <@>
2020-06-07 19:49:39 +0200

    first commit

---
+++ code/audio_processing.py
@@ -0,0 +1,53 @@
+import librosa
+import numpy as np
+
+
+input_length = 16000 * 10
+
+batch_size = 32
+
+n_mels = 256
+
+
+def pre_process_audio_mel_t(audio, sample_rate=16000):
+    mel_spec = librosa.feature.melspectrogram(y=audio, sr=sample_rate, n_mels=n_mels)
+    mel_db = (librosa.power_to_db(mel_spec, ref=np.max) + 40) / 40
+
+    return mel_db.T
+
+
+def load_audio_file(file_path, input_length=input_length):
+    data = librosa.core.load(file_path, sr=16000)[0]  # , sr=16000
+    if len(data) > input_length:
+
+        max_offset = len(data) - input_length
+
+        offset = np.random.randint(max_offset)
+
+        data = data[offset:(input_length + offset)]
+
+
+    else:
+        if input_length > len(data):
+            max_offset = input_length - len(data)
+
+            offset = np.random.randint(max_offset)
+        else:
+            offset = 0
+
+        data = np.pad(data, (offset, input_length - len(data) - offset), "constant")
+
+    data = pre_process_audio_mel_t(data)
+    return data
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    data = load_audio_file("Yung_Kartz_-_04_-_One_Way.mp3")
+
+    print(data.shape)
+
+    plt.imshow(data.T)
+    plt.show()
+
---
+++ code/models.py
@@ -0,0 +1,82 @@
+from tensorflow.keras.layers import (
+    Input,
+    GlobalMaxPool1D,
+    Dense,
+    Bidirectional,
+    GRU
+)
+from tensorflow.keras.losses import sparse_categorical_crossentropy
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+
+from transformer import Encoder
+
+
+def transformer_classifier(
+        num_layers=3,
+        d_model=256,
+        num_heads=8,
+        dff=512,
+        maximum_position_encoding=2048,
+        n_classes=16,
+):
+    inp = Input((None, d_model))
+
+    encoder = Encoder(
+        num_layers=num_layers,
+        d_model=d_model,
+        num_heads=num_heads,
+        dff=dff,
+        maximum_position_encoding=maximum_position_encoding,
+    )
+
+    x = encoder(inp)
+
+    x = GlobalMaxPool1D()(x)
+
+    out = Dense(n_classes, activation="softmax")(x)
+
+    model = Model(inputs=inp, outputs=out)
+
+    opt = Adam(0.00001, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+
+    model.compile(optimizer=opt, loss=sparse_categorical_crossentropy, metrics=["acc"])
+
+    model.summary()
+
+    return model
+
+
+def rnn_classifier(
+        d_model=256,
+        n_layers=2,
+        n_classes=16,
+):
+    inp = Input((None, d_model))
+
+    x = Bidirectional(GRU(d_model, return_sequences=True))(inp)
+
+    if n_classes > 1:
+        for i in range(n_layers - 1):
+            x = Bidirectional(GRU(d_model, return_sequences=True))(x)
+
+    x = GlobalMaxPool1D()(x)
+
+    out = Dense(n_classes, activation="softmax")(x)
+
+    model = Model(inputs=inp, outputs=out)
+
+    opt = Adam(0.00001, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+
+    model.compile(optimizer=opt, loss=sparse_categorical_crossentropy, metrics=["acc"])
+
+    model.summary()
+
+    return model
+
+
+if __name__ == '__main__':
+
+    model1 = transformer_classifier()
+
+    model2 = rnn_classifier()
